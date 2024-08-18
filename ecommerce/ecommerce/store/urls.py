4d04987193975eeb4b92c350b4b6e405f95cc7b1
Alireza <81501343+Alireza-ca7@users.noreply.github.com>
2024-08-18 16:29:34 +0330

    first version before having changing ability

---
+++ ecommerce/ecommerce/store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name = 'store'),
+    path('product/<slug:slug>/', views.product_info, name='product-info'), 
+    path('search/<slug:category_slug>/', views.list_category, name='list_category'), 
+]
